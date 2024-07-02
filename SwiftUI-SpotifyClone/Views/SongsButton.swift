ca0ec00262565fcff427de5417f027add97bddf4
stefanocasafranca <scasafrancal01@gmail.com>
2024-07-02 17:44:00 -0500

    Added the correct Gradient, Picture Cover, Initial Songs Buttons list including Icons

---
+++ SwiftUI-SpotifyClone/Views/SongsButton.swift
@@ -0,0 +1,18 @@
+//
+//  SongsButton.swift
+//  SwiftUI-SpotifyClone
+//
+//  Created by Stefano Casafranca Laos on 7/2/24.
+//
+
+import SwiftUI
+
+struct SongsButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SongsButton()
+}
