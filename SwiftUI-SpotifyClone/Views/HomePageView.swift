89508e7ee9e848b571b6eb11a556586016c99d33
stefanocasafranca <scasafrancal01@gmail.com>
2024-12-24 15:46:27 -0600

    Home Page View Background

--- SwiftUI-SpotifyClone/Views/HomePageView.swift
+++ SwiftUI-SpotifyClone/Views/HomePageView.swift
@@ -10,8 +10,14 @@ import SwiftUI
 struct HomePageView: View {
     var body: some View {
         // Layer 0
-       
+        LinearGradient(gradient: Gradient(colors: [Color(red: 61/255, green: 189/255, blue: 115/255), Color.black]),
+                       startPoint: .top, endPoint: .bottom)
+            .edgesIgnoringSafeArea(.all)
         
+        ScrollView {
+            Button(/*@START_MENU_TOKEN@*/"Button"/*@END_MENU_TOKEN@*/) {            }
+
+        }
         Text("This is the Home Page")
         .font(.largeTitle)
     }
